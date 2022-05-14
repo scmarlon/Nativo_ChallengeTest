426d6e1c26afd42456b9eee8cfc4c0898079d855
MARLON SÁNCHEZ CRUZ <marsc_97@hotmail.com>
2022-05-14 15:36:06 -0600

    Conection to the DB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://MarlonSac:marlon1221@cluster0.yiuar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, ()=> console.timeLog(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+    
+mongoose.set('strictQuery', false);
+
